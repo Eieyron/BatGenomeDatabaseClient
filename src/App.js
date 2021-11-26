5c46ff9701532b0f4eedaa70106aa71aa89eeff8
Aaron Magnaye <avmagnaye@up.edu.ph>
2021-11-26 12:50:02 +0800

    API Data to UPLB Server

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ if (localStorage.logged_in === undefined) {
 }
 
 // axios.defaults.baseURL = "http://0.0.0.0:8000/";
-axios.defaults.baseURL = "http://localhost:8000/";
+// axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "http://202.92.144.124:8000/";
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.access;
 
